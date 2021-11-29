cdca5e05fe42199446abfd1042b7ec7ad1cc4040
Priscy4163 <94042365+Priscy4163@users.noreply.github.com>
2021-11-29 13:32:16 -0800

    My commit

---
+++ Script_Priscy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# Name: Uchechukwu U.Priscillia
+
+# Email: Upriscillia5@gmail.com
+
+# Slack id: @Priscillia U. Faith
+
+# Area of interest: Proteomics
+
---
+++ Script_nasiba.R
@@ -0,0 +1,4 @@
+# Name: Nasiba Abdulkadir Ibrahim
+# Email: nasibaabdulkadir33@gmail.com
+# slack id: @nasiba a Ibrahim
+# Area of interest: Genomics
