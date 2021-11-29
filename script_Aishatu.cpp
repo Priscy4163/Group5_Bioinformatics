fc43d451a3ec420b238d63f2fca70ba4b5b11795
Priscy4163 <94042365+Priscy4163@users.noreply.github.com>
2021-11-29 14:10:18 -0800

    My commit

---
+++ script_Aishatu.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
+    cout << "\t\tHello aishatu Umar haliru:\n\n";
+    cout << "Name:"<< "\t\t\t" << "aishatu umar haliru\n";
+    cout << "Email:"<< "\t\t\t" << "aumarhaliru@gmail.com\n";
+    cout << "Slack_Id:" << "\t\t" << "@Aishatu_umar_haliru\n";
+    cout << "Area Of Interest:" << "\t" << "Genomics\n";
+    return 0;
+}
