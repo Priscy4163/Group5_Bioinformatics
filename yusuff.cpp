4c5d9b587cddae4824da0314cc696d8d3fd0a3fb
Priscy4163 <94042365+Priscy4163@users.noreply.github.com>
2021-11-30 06:47:48 -0800

    Delete yusuff.cpp

--- yusuff.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout<<"yusuf ibrahim"<<endl;
-    cout<<"talk2dagudi@gmail.com"<<endl;
-    cout<<"@yusuf ibrahim"<<endl;
-    cout<<"genetics"<<endl;
-}
