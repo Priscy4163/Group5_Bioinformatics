b88efebdd5e364c27c7e8a2cb7f8c9d8d27cd389
Priscy4163 <94042365+Priscy4163@users.noreply.github.com>
2021-11-30 06:52:06 -0800

    Delete script_khadija.py

--- script_khadija.py
+++
@@ -1,8 +0,0 @@
-#<-----------Abdullahi Project------------>
-print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
-
-print("\t\tHello Khadija Naseer, below are your informations:\n")
-print("Name: " + "\t\t\t" + "Khadija Naseer")
-print("Email: " + "\t\t\t"+ "khadijahnaseer67@gmail.com")
-print("Slack_username: " + "\t" + "@khadija_naseer")
-print("Area of interest: " + "\t" + "genomics")
