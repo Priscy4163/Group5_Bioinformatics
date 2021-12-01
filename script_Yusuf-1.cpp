87b1f9fbbfd2a193aa645bfa513583331f40dfc6
YUSUF  IBRAHIM <95091265+talkyusufibrahim@users.noreply.github.com>
2021-12-01 16:43:08 +0100

    Add files via upload

---
+++ script_Yusuf-1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
+    cout << "\t\tHello Yusuf Ibrahim:\n\n";
+    cout << "Name:"<< "\t\t\t" << "yusuf ibrahim\n";
+    cout << "Email:"<< "\t\t\t" << "talk2dagudi@gmail.com\n";
+    cout << "Slack_Id:" << "\t\t" << "@yusuf_ibrahim\n";
+    cout << "Area Of Interest:" << "\t" << "Genomics\n";
+    return 0;
+}
