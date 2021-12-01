e5a717d8664ecfc7f8520fd7ad771bfe65f41bbe
AUWAL  SHUAIBU ISA <95090225+AuwalShuaibu@users.noreply.github.com>
2021-12-01 23:39:25 +0100

    Add files via upload

---
+++ SCRIPTAUWAL[1].java
@@ -0,0 +1,10 @@
+#!/usr/bin/envj
+public class SCRIPTAUWAL{
+	public static void main(String[] args){
+		System.out.println("Name: AUWAL SHUAIBU ISA");
+		System.out.println("Email: auwalshuaibu680@gmail.com");
+		System.out.println("Slack Username: @Shuaibu A Isa");
+		System.out.println("Area of Interest: GENOMICS");
+		return 0;
+	}
+}
