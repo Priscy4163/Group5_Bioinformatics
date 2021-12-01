98ae33fee8a8c4821f3282b6615b2da6db62700a
Aumarhaliru <95125085+Aumarhaliru@users.noreply.github.com>
2021-12-01 20:14:49 +0100

    Add files via upload

---
+++ Aishatu.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main ()
+{
+    cout<<"Aishatu Umar Haliru"<<endl;
+    cout<<"Aumarhaliru@gmail.com"<<endl;
+    cout<<"@Aishatu Umar Haliru"<<endl;
+    cout<<"genomics"<<endl;
+}
