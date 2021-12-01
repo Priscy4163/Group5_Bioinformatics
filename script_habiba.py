10f413a6a1e43ae677a273e72996b77aa2c81b45
Khaery20 <95131251+Khaery20@users.noreply.github.com>
2021-12-01 21:54:55 +0100

    Add files via upload

---
+++ script_habiba.py
@@ -0,0 +1,9 @@
+#<-----------Adamu Project------------>
+print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+
+print("\t\tHello Habiba Adamu Usman, below are your informations:\n")
+print("Name: " + "\t\t\t" + "Habiba Adamu Usman")
+print("Email: " + "\t\t\t"+ "habibaadamuusman2@gmail.com")
+print("Slack_username: " + "\t" + "@Habiba_Adamu_usman")
+print("Area of interest: " + "\t" + "Metagenomics")
+
