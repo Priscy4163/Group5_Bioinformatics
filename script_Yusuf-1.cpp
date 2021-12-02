d50c8e71c2cf8c32335bd574d01fca3401c2d7b4
YUSUF  IBRAHIM <95091265+talkyusufibrahim@users.noreply.github.com>
2021-12-02 20:33:02 +0100

    Add files via upload

--- script_Yusuf-1.cpp
+++ script_Yusuf-1.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main()
 {
     cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
-    cout << "\t\tHello Yusuf Ibrahim:\n\n";
-    cout << "Name:"<< "\t\t\t" << "yusuf ibrahim\n";
-    cout << "Email:"<< "\t\t\t" << "talk2dagudi@gmail.com\n";
-    cout << "Slack_Id:" << "\t\t" << "@yusuf_ibrahim\n";
-    cout << "Area Of Interest:" << "\t" << "Genomics\n";
+    cout << "\t\tHello #,Yusuf Ibrahim:\n\n";
+    cout << "Name:"<< "\t\t\t" << "#yusuf ibrahim\n";
+    cout << "Email:"<< "\t\t\t" << "#talk2dagudi@gmail.com\n";
+    cout << "Slack_Id:" << "\t\t" << "#@yusuf_ibrahim\n";
+    cout << "Area Of Interest:" << "\t" << "#Genomics\n";
     return 0;
 }
